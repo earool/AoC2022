c550b3dac9c8e88c981de885a878695fe4869a41
earool <dawid.baranski0000@gmail.com>
2022-12-09 02:11:37 +0100

    Day Two: second part

--- day-2/dayTwo.mjs
+++ day-2/dayTwo.mjs
@@ -1,35 +1,29 @@
 import { open } from "node:fs/promises";
 
-/* 
-  A, B, C | X, Y, Z = Rock, Paper, Scissors
-  Rock, Paper, Scissors => 1, 2, 3
-  Win, Draw, Lose => 6, 3, 0
-*/
-
+// second-part
 const file = await open("./data.txt");
-let points = 0;
+let newPoints = 0;
 
 for await (const line of file.readLines()) {
   const myChoice = line.charAt(2);
   const elfsChoice = line.charAt(0);
-  const shapePoints =
-    myChoice === "X" ? 1 : myChoice === "Y" ? 2 : 3;
-  points += outcomeOfRound(myChoice, elfsChoice) + shapePoints;
+  const roundPoints = myChoice === 'X' ? 0 : myChoice === 'Y' ? 3 : 6;
+  newPoints += roundPoints + shapePoints(elfsChoice, myChoice);
 }
-console.log(points);
+
+console.log(newPoints);
 
 
-
-function outcomeOfRound(m, n) {
-  if (m === 'X') {
+function shapePoints(k, l) {
+  // k = elf's choice, l = outcome
+  if (k === 'A') {
     // Rock
-    return n === 'A' ? 3 : n === 'B' ? 0 : 6;
-  } else if (m === 'Y') {
+    return l === 'X' ? 3 : l === 'Y' ? 1 : 2;
+  } else if (k === 'B') {
     // Paper
-    return n === 'A' ? 6 : n === 'B' ? 3 : 0;
+    return l === 'X' ? 1 : l === 'Y' ? 2  : 3;
   } else {
     // Scissors
-    return n === 'A' ? 0 : n === 'B' ? 6 : 3;
+    return l === 'X' ? 2 : l === 'Y' ? 3 : 1;
   }
 }
-
